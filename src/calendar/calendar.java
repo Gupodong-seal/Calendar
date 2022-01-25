5d9ed5994f14837056c0f08aa1c1afa187c0cfb7
Gupodong-seal <Gupodong-seal@DESKTOP-UMLEOPG>
2022-01-25 21:43:27 +0900

    init calendar project

---
+++ src/calendar/calendar.java
@@ -0,0 +1,13 @@
+package calendar;
+
+public class calendar {
+	public static void main(String[] args) {
+
+		System.out.print(" 일 월  화  수 목  금 토\n");
+		System.out.print("--------------------\n");
+		System.out.print(" 1  2  3  4  5  6  7\n");
+		System.out.print(" 8  9 10 11 12 13 14\n");
+		System.out.print("15 16 17 18 19 20 21\n");
+		System.out.print("22 23 24 25 26 27 28\n");
+	}
+}
